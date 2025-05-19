e119f47ac8a47be3fae7ca7e9a568377f7b12303
dope66 <prince628@naver.com>
2025-05-19 17:43:00 +0900

    test

---
+++ src/main/java/kr/or/kosa/snippets/user/controller/pageController.java
@@ -0,0 +1,12 @@
+package kr.or.kosa.snippets.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class pageController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}

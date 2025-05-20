d469a05d39b6a3fe519f6aaaed3ef875d5bf923e
MinjiWoo <happyhippo171122@gmail.com>
2025-05-20 18:01:38 +0900

    fix : 4차 충돌 해결 시도

--- src/main/java/kr/or/kosa/snippets/user/config/RedisConfig.java
+++ src/main/java/kr/or/kosa/snippets/user/config/RedisConfig.java
@@ -1,7 +1,9 @@
 package kr.or.kosa.snippets.user.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -9,6 +11,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
     @Bean
+    @Primary
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);

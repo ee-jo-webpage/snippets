7a1c468a9698f7a65cae642c8f5d4c8fdf137215
MinjiWoo <happyhippo171122@gmail.com>
2025-05-22 16:31:31 +0900

    feat: 북마크 제거 API 구현

--- src/main/java/kr/or/kosa/snippets/tag/mapper/TagMapper.java
+++ src/main/java/kr/or/kosa/snippets/tag/mapper/TagMapper.java
@@ -12,12 +12,19 @@ import java.util.Map;
 @Repository("tagMapperBean")
 public interface TagMapper {
 
+    //모든 태그 조회
     @Select("SELECT * FROM tags ORDER BY name")
     List<TagItem> selectAllTags();
 
+    //태그이름으로 태그 조회
     @Select("SELECT * FROM tags WHERE name =#{name}")
     TagItem selectTagByName(String name);
 
+    //태그 ID로 태그 조회
+    @Select("SELECT * FROM tags WHERE tag_id = #{tagId}")
+    TagItem selectTagById(@Param("tagId") Long tagId);
+
+    //태그 생성
     @Insert("INSERT INTO tags (name) VALUES (#{name})")
     @Options(useGeneratedKeys = true, keyProperty = "tagId")
     int insertTag(TagItem tag);
@@ -31,4 +38,18 @@ public interface TagMapper {
 
     @Insert("INSERT INTO snippet_tags (snippet_id, tag_id) VALUES (#{snippetId}, #{tagId})")
     int insertSnippetTag(SnippetTag snippetTag);
+
+    //태그 제거
+    @Delete("DELETE FROM tags WHERE tag_id = #{tagId}")
+    void deleteTag(Long tagId);
+
+    //스니펫 태그 제거
+    @Delete("DELETE FROM snippet_tags WHERE snippet_id = #{snippetId} AND tag_id = #{tagId}")
+    int deleteSnippetTag(@Param("snippetId") Long snippetId, @Param("tagId") Long tagId);
+
+    //태그ID로 태그 제거
+    @Delete("DELETE FROM snippet_tags WHERE tag_id = #{tagId}")
+    int deleteSnippetTagsByTagId(Long tagId);
+
+
 }

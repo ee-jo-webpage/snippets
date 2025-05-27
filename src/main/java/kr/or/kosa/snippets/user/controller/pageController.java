6ac16eb646263f0361ff21f712a7161b701d80a0
dope66 <prince628@naver.com>
2025-05-27 18:25:36 +0900

    style(ui): 비밀번호 변경 페이지 input 태그 css 보완 , 및 pageController Popularsnippet 가져오기

--- src/main/java/kr/or/kosa/snippets/user/controller/pageController.java
+++ src/main/java/kr/or/kosa/snippets/user/controller/pageController.java
@@ -1,5 +1,6 @@
 package kr.or.kosa.snippets.user.controller;
 
+import kr.or.kosa.snippets.snippetExt.service.SnippetExtService;
 import kr.or.kosa.snippets.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -11,7 +12,10 @@ import java.security.Principal;
 @Controller
 @RequiredArgsConstructor
 public class pageController {
+
     private final UserService userService;
+    private final SnippetExtService snippetExtService;
+
     @GetMapping("/")
     public String index(Model model, Principal principal) {
         if (principal != null) {
@@ -19,6 +23,9 @@ public class pageController {
             String nickname = userService.getNicknameByEmail(email);
             model.addAttribute("nickname", nickname);
         }
+
+        model.addAttribute("topSnippets", snippetExtService.getTop3PopularSnippets());
+        System.out.println(snippetExtService.getTop3PopularSnippets());
         return "main";
     }
 }

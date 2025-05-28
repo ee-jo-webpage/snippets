f6b07dadd7eec0794988c58f83d2ad6311501a23
jarajiri <eic3021@gmail.com>
2025-05-28 17:22:49 +0900

    feat: 관리자로 로그인시 메인페이지에서 IP 차단 대시보드로 이동하는 분기문 추가 - main.html - pageController 에 반영

--- src/main/java/kr/or/kosa/snippets/user/controller/pageController.java
+++ src/main/java/kr/or/kosa/snippets/user/controller/pageController.java
@@ -1,13 +1,16 @@
 package kr.or.kosa.snippets.user.controller;
 
 import kr.or.kosa.snippets.snippetExt.service.SnippetExtService;
+import kr.or.kosa.snippets.user.service.CustomUserDetails;
 import kr.or.kosa.snippets.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.security.Principal;
+import java.util.Collection;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,15 +20,19 @@ public class pageController {
     private final SnippetExtService snippetExtService;
 
     @GetMapping("/")
-    public String index(Model model, Principal principal) {
-        if (principal != null) {
-            String email = principal.getName();
+    public String index(Model model, @AuthenticationPrincipal CustomUserDetails details) {
+        if (details != null) {
+            String email = details.getName();
             String nickname = userService.getNicknameByEmail(email);
             model.addAttribute("nickname", nickname);
+
+            boolean isAdmin = details.getAuthorities().stream()
+                .anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN"));
+            model.addAttribute("isAdmin", isAdmin);
         }
 
         model.addAttribute("topSnippets", snippetExtService.getTop3PopularSnippets());
-        System.out.println(snippetExtService.getTop3PopularSnippets());
         return "main";
     }
+
 }
